53c5452d6923f1715d9fc8267e74e5efbdce62a8
Murat Ertik <dmuratertik1@gmail.com>
2026-04-23 00:16:55 +0300

    first commit

---
+++ src/test/java/org/testdrivendevelopment/services/OrderServiceTests.java
@@ -0,0 +1,20 @@
+package org.testdrivendevelopment.services;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTests {
+
+    @Test
+    public void it_should_create_a_new_order() {
+
+        //given
+
+
+        //when
+
+        OrderService service = new OrderService();
+
+
+        //then
+    }
+}
