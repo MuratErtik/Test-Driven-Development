63b2c21196da1d656eca90456f07129299b41856
Murat Ertik <dmuratertik1@gmail.com>
2026-04-23 01:01:32 +0300

    then(order.getTotalPrice()).isEqualTo() passed for constant value

--- src/main/java/org/testdrivendevelopment/services/OrderService.java
+++ src/main/java/org/testdrivendevelopment/services/OrderService.java
@@ -2,8 +2,20 @@ package org.testdrivendevelopment.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.testdrivendevelopment.dtos.CreateOrderRequest;
+import org.testdrivendevelopment.dtos.OrderDto;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+
+
+    public OrderDto createOrder(CreateOrderRequest request) {
+
+        return  OrderDto.builder()
+                .totalPrice(BigDecimal.valueOf(99.95))
+                .build();
+    }
 }
--- src/test/java/org/testdrivendevelopment/services/OrderServiceTests.java
+++ src/test/java/org/testdrivendevelopment/services/OrderServiceTests.java
@@ -1,6 +1,13 @@
 package org.testdrivendevelopment.services;
 
 import org.junit.jupiter.api.Test;
+import org.testdrivendevelopment.dtos.CreateOrderRequest;
+import org.testdrivendevelopment.dtos.OrderDto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 public class OrderServiceTests {
 
@@ -8,13 +15,27 @@ public class OrderServiceTests {
     public void it_should_create_a_new_order() {
 
         //given
+        OrderService service = new OrderService();
+        CreateOrderRequest request = CreateOrderRequest.builder()
+                .amount(5)
+                .customerCode("code1")
+                .unitPrice(BigDecimal.valueOf(19.99))
+                .build();
+
+
 
 
         //when
 
-        OrderService service = new OrderService();
+        OrderDto order = service.createOrder(request);
 
 
         //then
+
+        then(order).isNotNull(); // at first createOrder method returns null that's mean you got a failed test
+        //so you can start to develop prod code
+
+
+        then(order.getTotalPrice()).isEqualTo(BigDecimal.valueOf(19.99).multiply(BigDecimal.valueOf(5)).setScale(2, RoundingMode.HALF_UP));
     }
 }
