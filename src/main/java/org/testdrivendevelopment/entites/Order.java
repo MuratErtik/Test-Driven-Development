71b87acd43cf891b04c407ffe07d727db064124b
Murat Ertik <dmuratertik1@gmail.com>
2026-04-23 16:32:12 +0300

    order entity created.

---
+++ src/main/java/org/testdrivendevelopment/entites/Order.java
@@ -0,0 +1,27 @@
+package org.testdrivendevelopment.entites;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal totalPrice;
+
+
+}
