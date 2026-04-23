331fa87dc3fdbfa727f8203c426b41ad7fcd07b1
Murat Ertik <dmuratertik1@gmail.com>
2026-04-23 18:55:25 +0300

    payment client created.

---
+++ src/main/java/org/testdrivendevelopment/clients/PaymentClient.java
@@ -0,0 +1,9 @@
+package org.testdrivendevelopment.clients;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentClient {
+    public void pay(Object any) {
+    }
+}
