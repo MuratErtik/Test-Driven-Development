b1302382a2de4671c3943540c3b9097a81141d4b
Murat Ertik <dmuratertik1@gmail.com>
2026-04-23 16:32:55 +0300

    order repo added and inject into creteOrder method.

--- src/main/java/org/testdrivendevelopment/services/OrderService.java
+++ src/main/java/org/testdrivendevelopment/services/OrderService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.testdrivendevelopment.dtos.CreateOrderRequest;
 import org.testdrivendevelopment.dtos.OrderDto;
+import org.testdrivendevelopment.entites.Order;
+import org.testdrivendevelopment.repositories.OrderRepository;
 
 import java.math.BigDecimal;
 
@@ -11,14 +13,22 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
 
     public OrderDto createOrder(CreateOrderRequest request) {
 
         BigDecimal totalPrice = request.getUnitPrice().multiply(BigDecimal.valueOf(request.getAmount()));
 
+        Order order = Order.builder()
+                .totalPrice(totalPrice)
+                .build();
+
+        Order savedOrder = orderRepository.save(order);
+
 
         return  OrderDto.builder()
                 .totalPrice(totalPrice)
+                .orderId(savedOrder.getId())
                 .build();
     }
 }
